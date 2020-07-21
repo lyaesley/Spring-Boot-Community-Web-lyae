bae0818762fac4373cc9e9ff0caf6447490d79f9
lyae <lyaesley@gmail.com>
2020-07-22 01:32:01 +0900

    5장 예제 5-18 진행중

--- src/main/java/com/web/LyaeApplication.java
+++ src/main/java/com/web/LyaeApplication.java
@@ -5,19 +5,32 @@ import com.web.domain.User;
 import com.web.domain.enums.BoardType;
 import com.web.repository.BoardRepository;
 import com.web.repository.UserRepository;
+import com.web.resolver.UserArgumentResolver;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.stream.IntStream;
 
 @SpringBootApplication
-public class LyaeApplication {
+public class LyaeApplication extends WebMvcConfigurerAdapter {
 
     public static void main(String[] args) {
         SpringApplication.run(LyaeApplication.class, args);
+    }
+
+    @Autowired
+    private UserArgumentResolver userArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(userArgumentResolver);
     }
 
     //처음 배우는 스프링 부트 2 책 내용 page.112  //옛날방식인듯.. 최신방식 @EventListener (재현하려 했으나 bean 주입하는 법을 몰라서 포기
---
+++ src/main/java/com/web/resolver/UserArgumentResolver.java
@@ -0,0 +1,62 @@
+package com.web.resolver;
+
+import com.web.annotation.SocialUser;
+import com.web.domain.User;
+import com.web.repository.UserRepository;
+import org.springframework.core.MethodParameter;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class UserArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private UserRepository userRepository;
+
+    public UserArgumentResolver(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.getParameterAnnotation(SocialUser.class) != null &&
+                parameter.getParameterType().equals(User.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
+                                  NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        HttpSession session = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest().getSession();
+        User user = (User) session.getAttribute("user");
+        return getUser(user, session);
+    }
+
+    private User getUser(User user, HttpSession session) {
+        if(user == null) {
+            try {
+                OAuth2Authentication authentication = (OAuth2Authentication) SecurityContextHolder.getContext().getAuthentication();
+                Map<String, String> map = (HashMap<String, String>) authentication.getUserAuthentication().getDetails();
+                User converUser = converUser(String.valueOf(authentication.getAuthorities().toArray()[0]), map);
+
+                user = userRepository.findByEmail(converUser.getEmail());
+                if(user == null) { user = userRepository.save(converUser); }
+
+                setRoleIfNotSame(user, authentication, map);
+                session.setAttribute("user", user);
+            } catch (ClassCastException e) {
+                return user;
+            }
+        }
+        return user;
+    }
+}
+
